5b8c0028d506aa27cc756390860916d03310330f
Rajashekar Vemula <vemula.rajashekar02@gmail.com>
2022-10-31 12:04:44 +0530

    added LinkedList Programs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructureLinkedList
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList program");
+        }
+    }
+}
