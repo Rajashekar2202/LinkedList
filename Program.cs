adfddcb2a47ccd119e4534f67c02357bce9ac1e6
Rajashekar Vemula <vemula.rajashekar02@gmail.com>
2022-10-31 12:57:17 +0530

    delete 40 after 30 in list

--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@ namespace DataStructureLinkedList
             list.Add(70);
             list.Display();
             Console.WriteLine("\nAdd after 30 : 40");
+            int position = list.DeletePerticulatElement(30);
+            Console.WriteLine("\nthe size of linkedlist is {0} ", position);
             list.AddAfterAtPerticulatPosition(30);
             list.Display();
             Console.Read();
